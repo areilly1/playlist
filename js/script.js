7fabf6969154c4e8aac550e148936c8593956767
Glitch (playlistwebsite) <none>
2021-02-25 23:50:58 +0000

    🍰🍓 Updated with Glitch

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var songNames = ["Boo'd Up", "Nice For What", "Universe"];
+var songArtists = ["Ella Mai", "Drake", "EXO"];
+var songLength = ["241", "201", "289"];
+var images = ["https://i.ytimg.com/vi/hCR3UbyQ9jc/maxresdefault.jpg", 
+              "https://i.ytimg.com/vi/KRaPykuwCiw/maxresdefault.jpg;maxHeight=145;maxWidth=222",
+              "https://pm1.narvii.com/6691/5b465c2eea230b1024fbc27156b7eefabcc77069_hq.jpg"]
+var links = ["https://www.youtube.com/watch?v=6YNZlXfW6Ho",
+            "https://www.youtube.com/watch?v=KRaPykuwCiw",
+            "https://www.youtube.com/watch?v=leu-cTvMWTA"]
+
+
